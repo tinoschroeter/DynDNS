fb47c0c309cad6209b1b7cec9d5512db25b5d572
Tino Schroter <tino.schroeter@gmail.com>
2022-03-12 23:20:35 +0100

    fix: Interpolation-only expressions are deprecated

--- route53/tino.sh/argo-tino-sh.tf
+++ route53/tino.sh/argo-tino-sh.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "argo" {
   zone_id = data.aws_route53_zone.tino.zone_id
   name    = "argo.${data.aws_route53_zone.tino.name}"
   type    = "CNAME"
-  ttl     = "${var.ttl}"
+  ttl     = var.ttl
   records = ["tino.sh."]
 }
 
