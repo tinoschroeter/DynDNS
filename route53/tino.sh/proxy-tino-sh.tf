5884487204396858582a14ec13f5650779f2870a
Tino Schroter <tino.schroeter@gmail.com>
2022-04-13 22:26:58 +0200

    remove: variable interpolation

--- route53/tino.sh/proxy-tino-sh.tf
+++ route53/tino.sh/proxy-tino-sh.tf
@@ -2,6 +2,6 @@ resource "aws_route53_record" "proxy" {
   zone_id = data.aws_route53_zone.tino.zone_id
   name    = "proxy.${data.aws_route53_zone.tino.name}"
   type    = "CNAME"
-  ttl     = "${var.ttl}"
+  ttl     = var.tt
   records = ["tino.sh."]
 }
