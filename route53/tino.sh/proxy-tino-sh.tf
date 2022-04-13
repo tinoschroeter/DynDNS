0cb9fea42b0aac0b42843cb98bb295ce537801be
Tino Schroter <tino.schroeter@gmail.com>
2022-04-13 22:29:43 +0200

    fix: variable typo

--- route53/tino.sh/proxy-tino-sh.tf
+++ route53/tino.sh/proxy-tino-sh.tf
@@ -2,6 +2,6 @@ resource "aws_route53_record" "proxy" {
   zone_id = data.aws_route53_zone.tino.zone_id
   name    = "proxy.${data.aws_route53_zone.tino.name}"
   type    = "CNAME"
-  ttl     = var.tt
+  ttl     = var.ttl
   records = ["tino.sh."]
 }
