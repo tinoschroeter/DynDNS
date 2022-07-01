fd3e16937a9edec1cafbccd7639857ce27027114
Tino Schroter <tino.schroeter@gmail.com>
2022-07-01 20:55:47 +0200

    add: alice subdomain

---
+++ route53/tino.sh/alice-tino-sh.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "alice" {
+  zone_id = data.aws_route53_zone.tino.zone_id
+  name    = "alice.${data.aws_route53_zone.tino.name}"
+  type    = "CNAME"
+  ttl     = "${var.ttl}"
+  records = ["tino.sh."]
+}
