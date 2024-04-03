54bff36642f583309dd31458ce1fed632b4595a3
Tino Schröter <tino.schroeter@eventim.de>
2024-04-03 18:55:12 +0200

    remove: mongodb setup

--- cloudRun/index.js
+++ cloudRun/index.js
@@ -1,7 +1,5 @@
 "use strict";
-const { MongoClient } = require("mongodb");
 const morgan = require("morgan");
-const getIP = require("./routes/getIP");
 const setIP = require("./routes/setIP");
 
 const API_KEY = process.env.API_KEY;
@@ -9,27 +7,13 @@ const API_KEY = process.env.API_KEY;
 const express = require("express");
 const app = express();
 
-const uri = process.env.MONGO_URI
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("combined"));
 
-MongoClient(uri, options)
-  .connect()
-  .then((client) => {
-    const collection = client.db("ops").collection("ip");
+app.post("/" + API_KEY, setIP(collection));
 
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(morgan("combined"));
-
-    app.get("/", getIP(collection));
-    app.post("/" + API_KEY, setIP(collection));
-
-    const port = process.env.PORT || 8080;
-    app.listen(port, () => {
-      console.log(`Server is running on Port ${port}`);
-    });
-  })
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on Port ${port}`);
+});
--- cloudRun/routes/setIP.js
+++ cloudRun/routes/setIP.js
@@ -3,40 +3,20 @@ const sendMessageToSlackChannel = require("../utils/sendMessageToSlackChannel");
 const updateDNS = require("../utils/updateDNS");
 const net = require("net");
 
-const { format } = require("date-fns");
-
-const setIP = (collection) => {
+const setIP = () => {
   return (req, res) => {
     const ip =
       req.headers["x-forwarded-for"] ||
       req.connection.remoteAddress.toString().replace("::ffff:", "");
 
-    console.log("ip: ", ip);
-    collection
-      .findOne({}, { sort: { $natural: -1 } })
-      .then((result) => {
-        if (net.isIPv4(ip) && result.ip !== ip) {
-          collection
-            .insertOne({ ip: ip, date: format(new Date(), "dd/MM/yyyy-HH:mm") })
-            .then(() => {
-              const channelName = "#dyndns";
-              sendMessageToSlackChannel(
-                `Deine neue IP ist: ${ip}`,
-                channelName
-              );
-              updateDNS(ip).catch((err) => console.error(err));
-              console.log("update ip...");
+    if (net.isIPv4(ip)) {
+      const channelName = "#dyndns";
+      sendMessageToSlackChannel(`Deine neue IP ist: ${ip}`, channelName);
+      updateDNS(ip).catch((err) => console.error(err));
+      console.log(`update ip to ${ip}`);
 
-              res.status(201).json({ message: `${ip} added to database` });
-            });
-        } else {
-          res.status(200).json({ message: `${result.ip} no ip change` });
-        }
-      })
-      .catch((e) => {
-        console.error(e);
-        res.status(500).end();
-      });
+      res.status(200).json({ message: `${ip} updated` });
+    }
   };
 };
 
