d1c3792430539109ba0730e5841255a09fa26957
Tino Schröter <tino.schroeter@eventim.de>
2024-04-03 19:05:37 +0200

    update: deploy script

--- cloudRun/deploy.sh
+++ cloudRun/deploy.sh
@@ -2,10 +2,9 @@
 
 gcloud builds submit --tag gcr.io/gke-dev-307616/ipservice
 gcloud run deploy \
-  --set-env-vars API_KEY="" \
-  --set-env-vars MONGO_URI="" \
-  --set-env-vars SLACK_TOKEN="" \
-  --set-env-vars accessKeyId="" \
-  --set-env-vars secretAccessKey="" \
-  --set-env-vars hostedzoneId="" \
-  --image gcr.io/gke-dev-307616/ipservice --platform managed
+	--set-env-vars API_KEY="" \
+	--set-env-vars SLACK_TOKEN="" \
+	--set-env-vars accessKeyId="" \
+	--set-env-vars secretAccessKey="" \
+	--set-env-vars hostedzoneId="" \
+	--image gcr.io/gke-dev-307616/ipservice --platform managed
